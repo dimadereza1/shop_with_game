ce7bc5239afa29f107c3f9caf929f638a7ecadb7
I_am_jumper_My_job_is__to_jump <dimadereza1@gmail.com>
2023-05-16 21:34:56 +0300

    added admin(__init__.py, for_admin.html): added function for Admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, flash, url_for
 from .models import User, Products, db
-from .forms import LoginForm, RegForm
+from .forms import LoginForm, RegForm, AdminForm
 from flask_login import LoginManager, logout_user, current_user, login_user
 from .config import app
 
@@ -33,7 +33,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_pass(form.password.data):
             flash('Неправильний пароль a6o логін ')
-            return redirect(url_for('login'))
+            return redirect(url_for("Login"))
         login_user(user)
         return redirect('index')
     return render_template('login.html', title='login', form=form) 
@@ -53,3 +53,19 @@ def reg():
 @app.route('/prodile')
 def prof():
     return render_template('profile.html')
+
+@app.route('/for_admin', methods=['GET', 'POST'])
+def for_ad():
+    form = AdminForm()
+    if current_user.username == 'Admin':
+        if form.validate_on_submit():
+            add_prod = Products(name_prod=form.name_prod.data, price=form.price.data, producer=form.producer.data, year=form.year.data)
+            db.session.add(add_prod)
+            db.session.commit()
+    else:
+        flash('Ви не є адміном!!!')
+    return render_template('for_admin.html', form=form, title='Додавання товарів')
+
+@app.route('/basket')
+def prof():
+    return render_template('basket.html')
--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, IntegerField
 from wtforms.validators import DataRequired
 
 class RegForm(FlaskForm):
@@ -12,3 +12,11 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class AdminForm(FlaskForm):
+    name_prod = StringField('Name of product', validators=[DataRequired()])
+    price = IntegerField('Price', validators=[DataRequired()])
+    genre = StringField('Genre', validators=[DataRequired()])
+    producer = StringField('Producer', validators=[DataRequired()])
+    year = IntegerField('Year', validators=[DataRequired()])
+    submit = SubmitField('Підтвердити')
