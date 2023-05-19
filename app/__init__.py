05ff3d3fa64fe0914cda714225a6c6eaf7821a73
I_am_jumper_My_job_is__to_jump <dimadereza1@gmail.com>
2023-05-19 21:53:48 +0300

    added catalog(many files): added working cataloges

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from .models import User, Products, db
 from .forms import LoginForm, RegForm, AdminForm
 from flask_login import LoginManager, logout_user, current_user, login_user
 from .config import app
-
+from sqlalchemy import desc
 
 login = LoginManager(app)
 
@@ -21,8 +21,68 @@ def logout():
 @app.route('/', methods=['GET', 'POST']) 
 @app.route('/index', methods=['GET', 'POST'])
 def main():
-    Produt = Products.query.all()
-    return render_template('index.html', title='BG', prod=Produt)
+    Produt_All = Products.query.all()
+    return render_template('index.html', title='BG', prod=Produt_All)
+
+@app.route('/sorted_by_year_h', methods=['GET', 'POST'])
+def main1():
+    Produt_Year = Products.query.order_by(desc(Products.year)).all()
+    return render_template('index_y_h.html', title='BG', prod=Produt_Year)
+
+@app.route('/sorted_by_year_l', methods=['GET', 'POST'])
+def main2():
+    Produt_Year = Products.query.order_by(Products.year).all()
+    return render_template('index_y_l.html', title='BG', prod=Produt_Year)
+
+@app.route('/sorted_by_price_h', methods=['GET', 'POST'])
+def main3():
+    Produt_Price = Products.query.order_by(desc(Products.price)).all()
+    return render_template('index_p_h.html', title='BG', prod=Produt_Price)
+
+@app.route('/sorted_by_price_l', methods=['GET', 'POST'])
+def main4():
+    Produt_Price = Products.query.order_by(Products.price).all()
+    return render_template('index_p_l.html', title='BG', prod=Produt_Price)
+
+@app.route('/simulators', methods=['GET', 'POST'])
+def main5():
+    all = Products.query.all()
+    sim = []
+    for i in all:
+        for b in i.genre.split(','):
+            if b == 'Simulator':
+                sim.append(i)
+    return render_template('index_sim.html', title='BG', prod=sim)
+
+@app.route('/racing', methods=['GET', 'POST'])
+def main6():
+    all = Products.query.all()
+    sim = []
+    for i in all:
+        for b in i.genre.split(','):
+            if b == 'Racing':
+                sim.append(i)
+    return render_template('index_rc.html', title='BG', prod=sim)
+
+@app.route('/action', methods=['GET', 'POST'])
+def main7():
+    all = Products.query.all()
+    sim = []
+    for i in all:
+        for b in i.genre.split(','):
+            if b == 'Action':
+                sim.append(i)
+    return render_template('index_action.html', title='BG', prod=sim)
+
+@app.route('/horror', methods=['GET', 'POST'])
+def main8():
+    all = Products.query.all()
+    sim = []
+    for i in all:
+        for b in i.genre.split(','):
+            if b == 'Horror':
+                sim.append(i)
+    return render_template('index_hr.html', title='BG', prod=sim)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -33,7 +93,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_pass(form.password.data):
             flash('Неправильний пароль a6o логін ')
-            return redirect(url_for("Login"))
+            return redirect(url_for("login"))
         login_user(user)
         return redirect('index')
     return render_template('login.html', title='login', form=form) 
@@ -59,13 +119,14 @@ def for_ad():
     form = AdminForm()
     if current_user.username == 'Admin':
         if form.validate_on_submit():
-            add_prod = Products(name_prod=form.name_prod.data, price=form.price.data, producer=form.producer.data, year=form.year.data)
+            add_prod = Products(name_prod=form.name_prod.data, price=form.price.data, producer=form.producer.data, year=form.year.data, genre=form.genre.data)
             db.session.add(add_prod)
             db.session.commit()
+            return redirect('index')
     else:
         flash('Ви не є адміном!!!')
     return render_template('for_admin.html', form=form, title='Додавання товарів')
 
 @app.route('/basket')
-def prof():
+def basket():
     return render_template('basket.html')
