8361e4602282f7940709feb51c06d43f46c4a3bc
I_am_jumper_My_job_is__to_jump <dimadereza1@gmail.com>
2023-05-27 14:46:11 +0300

    TypeError

--- app/models.py
+++ app/models.py
@@ -32,11 +32,3 @@ class Products(db.Model):
     producer = db.Column(db.String, index=True)
     year = db.Column(db.Integer, index=True)
 
-class Basket_db(db.Model):
-    __tablename__ = 'Basket'
-    id = db.Column(db.Integer, primary_key=True)
-    name_prod = db.Column(db.String(120), index=True, unique=True)
-    price = db.Column(db.Integer)
-    genre = db.Column(db.String, index=True)
-    producer = db.Column(db.String, index=True)
-    year = db.Column(db.Integer, index=True)
